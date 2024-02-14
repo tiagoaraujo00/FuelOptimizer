cfe7efece7dccfceac35ed1f335134f0c474c8c4
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-02-14 14:56:19 -0300

    feat: define fuel done!

--- app/src/main/java/com/tiago/fueloptimizer/MainActivity.kt
+++ app/src/main/java/com/tiago/fueloptimizer/MainActivity.kt
@@ -15,12 +15,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tiago.fueloptimizer.ui.theme.FuelOptimizerTheme
@@ -44,6 +47,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
+
+    var gasolineValue by remember {
+        mutableStateOf("")
+    }
+
+    var alcoholValue by remember {
+        mutableStateOf("")
+    }
+
     Column(
         Modifier
             .background(color = Color(0xFF009688))
@@ -59,35 +71,38 @@ fun App() {
                 text = "Álcool ou Gasolina",
                 style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp)
             )
-            Text(
-                text = "Gasolina", style = TextStyle(
-                    color = Color.Red,
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold
+
+            if (alcoholValue.isNotBlank() && gasolineValue.isNotBlank()) {
+                val gasolineOrAlcohol = alcoholValue.toDouble() / gasolineValue.toDouble() > 0.7
+                val fuelDefine = if (gasolineOrAlcohol) "Gasolina" else "Alcool"
+                val colorDefine = if (gasolineOrAlcohol) Color.Red else Color.Blue
+                Text(
+                    text = fuelDefine, style = TextStyle(
+                        color = colorDefine,
+                        fontSize = 40.sp,
+                        fontWeight = FontWeight.Bold
+                    )
                 )
-            )
+            }
+
             TextField(
-                value = "2.00",
-                onValueChange = {},
+                value = gasolineValue,
+                onValueChange = {
+                    gasolineValue = it
+                },
                 label = {
-                        Text(text = "Gasolina")
+                    Text(text = "Gasolina")
                 },
             )
             TextField(
-                value = "2.00",
-                onValueChange = {},
+                value = alcoholValue,
+                onValueChange = {
+                    alcoholValue = it
+                },
                 label = {
                     Text(text = "Álcool")
                 },
             )
         }
-    }
-}
-
-@Preview
-@Composable
-fun appPreview() {
-    FuelOptimizerTheme {
-        App()
     }
 }
